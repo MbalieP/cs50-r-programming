1b1ee69890541255e5873aae426f195095ddbaff
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-25 07:15:17 +0200

    sum

--- tabulate.R
+++ tabulate.R
@@ -5,4 +5,17 @@ vote <- read.csv(
   )
 vote[1,2]
 vote[,2]
-vote$mail
+vote$poll[1]
+
+sum(vote$poll[1],vote$poll[2],vote$poll[3])
+
+sum(vote$mail)
+
+vote$poll[1] + votes$mail[1]
+vote$poll[2] + votes$mail[2]
+vote$poll[3] + votes$mail[3]
+
+vote$poll + votes$mail
+vote[,2] + vote[,3] 
+
+vote$total <- vote$poll + votes$mail
