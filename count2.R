058632d72e7dd63c515a933b989da93cfbd27157
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-25 16:04:20 +0200

    lecture 3

---
+++ count2.R
@@ -0,0 +1,13 @@
+get_votes <- function(prompt = " Enter votes: ") {
+  votes <- as.integer(readline(prompt))
+ # return(votes) 
+  
+
+}
+  
+  mario <- get_votes("Mario: ")
+  peach <- get_votes("Peach: ")
+  bowser <- get_votes("Bowser: ")
+  
+  total <- sum(mario, peach, bowser)
+  cat("Total votes:", total)
