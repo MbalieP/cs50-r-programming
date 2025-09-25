7bff8b5718f224ab9c362ec997b77621e2ee64bd
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-25 16:32:11 +0200

    LECTURE 2

--- count2.R
+++ count2.R
@@ -1,16 +1,11 @@
-get_votes <- function(prompt = " Enter votes: ") {
-  votes <- supressessWarnings(as.integer(readline(prompt)))
-  if(is.na(votes)){
-    return(0)
-  }else{
-    return(votes)
-  }
-
+get_votes <- function(prompt = "Enter votes: ") {
+  votes <- suppressWarnings(as.integer(readline(prompt)))
+  ifelse(is.na(votes), 0, votes)
 }
-  
-  mario <- get_votes(prompt = "Mario: ")
-  peach <- get_votes(prompt = "Peach: ")
-  bowser <- get_votes(prompt = "Bowser: ")
-  
-  total <- sum(mario, peach, bowser)
-  cat("Total votes:", total)
+
+mario <- get_votes(prompt = "Mario: ")
+peach <- get_votes(prompt = "Peach: ")
+bowser <- get_votes(prompt = "Bowser: ")
+
+total <- sum(mario, peach, bowser)
+cat("Total votes:", total)
