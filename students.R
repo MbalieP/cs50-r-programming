4fb69a7eb92ff215d87eed6714e17d27ded08c51
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-26 15:18:08 +0200

    pivot data

--- students.R
+++ students.R
@@ -1,9 +1,15 @@
 students <- read.csv("students.csv")
 View(students)
 
-pivot_wider(
+students <- pivot_wider(
   students,
   id_cols = student,
   names_from = attributes,
   values_from = value
 )
+
+students$GPA <- as.numeric(students$GPA)
+
+students |>
+  group_by(major) |>
+  summarize(GPA = mean(GPA))
