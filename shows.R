0cbd11d2e63d14f793e15343f579fedcd2935d12
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-26 18:27:02 +0200

    end of lecture 4

--- shows.R
+++ shows.R
@@ -1,5 +1,13 @@
 shows <- read.csv("shows.csv")
 
+show$show <- show$show |>
+  str_trim() |>
+  str_squish() |>
+  str_to_title() 
+
+
+shows$shows[str_detect(shows$show,"Avatar")] <- "avatar the last airbender"
+
 shows |>
   group_by(show) |>
   summarize(votes = n()) |>
