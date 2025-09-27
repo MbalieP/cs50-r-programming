0d7074e40d37a6de4708e15256bd234d7cd971f8
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-27 07:04:16 +0200

    Lecture 5

--- votes3.R
+++ votes3.R
@@ -1,4 +1,10 @@
 votes <- read.csv("votes.csv")
 View(votes)
 
-ggplot()
+ggplot(votes,aes(x = candidate, y = votes)) + 
+  geom_col()
+
+# overriding the defaults to make it more apealling
+#Scales :continous(limits) discret
+
+
