afd0b024c5fdbaf7fd2b3e3e60891890fad6e783
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-27 07:23:14 +0200

    Lecture 5

--- votes3.R
+++ votes3.R
@@ -2,7 +2,15 @@ votes <- read.csv("votes.csv")
 View(votes)
 
 ggplot(votes,aes(x = candidate, y = votes)) + 
-  geom_col()
+  geom_col(aes(fill = candidate)) + 
+  scale_fill_viridis_d("Candidate") + 
+  scale_y_continuous(limits = c(0,250)) +
+  labs(
+    x = "Candidates",
+    y = "Votes",
+    title = "Election Results"
+  ) + 
+  theme_classic()
 
 # overriding the defaults to make it more apealling
 #Scales :continous(limits) discret
