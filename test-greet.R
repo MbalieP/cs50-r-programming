95b757c14b99e9c35c21cc60a2f895bd8a95ee75
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-28 09:33:18 +0200

    test driven behavior

---
+++ test-greet.R
@@ -0,0 +1,18 @@
+source("greet.R")
+
+test_that("`greet` says hello to a user",{
+  expect_equal(greet("Carter"),"hello,Carter")
+  expect_equal(greet("Mario"),"hello,Mario")
+  expect_equal(greet("Peach"),"hello,Peach")
+})
+
+#BEHAVIOR DRIVEN DEVELOPMENT
+#describe
+#it
+
+describe("greet()",{
+  it("can say hello to a user",{
+    name <- "Carter"
+    expect_equal(greet(name), "hello, Carter")
+  })
+})
