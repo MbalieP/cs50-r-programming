c70b45e3c519051a58c992dec7f6beb112b5bbce
Mbali Phulwane <mbphulwajhb024@student.wethinkcode.co.za>
2025-09-28 08:07:21 +0200

    warnings

---
+++ test-average.R
@@ -0,0 +1,28 @@
+#UNIT TESTS
+# to test some individual function in our code
+source("average.R")
+
+test_average <- function(){
+  if(average(c(1,2,3)) == 2){
+    cat("`average` passed test :)\n")
+  }else{
+    cat("`average` failed test :(\n")
+  }
+  
+  
+  
+  if(average(c(-1,-2,-3)) == -2){
+    cat("`average` passed test :)\n")
+  }else{
+    cat("`average` failed test :(\n")
+  }
+  
+  
+  if(average(c(-1,0,1)) == -2){
+    cat("`average` passed test :)\n")
+  }else{
+    cat("`average` failed test :(\n")
+  }
+}
+
+test_average()
