cabb03dc7fb401e8602c9ec75caf96aecca6946f
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-30 06:18:16 +0200

    Update count2.R

--- Lecture3_Functions/count2.R
+++ Lecture3_Functions/count2.R
@@ -1,12 +1,5 @@
 
-    # If the input is not a valid number, repeat the prompt
-  }
-}  
- # Initialize a variable to keep track of the total votes
-total <- 0
 
-# Loop over each character name and collect votes
-for (i in c("Mario", "Peach", "Bowser")) { 
 
  Ask for votes using the get_votes function and pass a custom prompt
   votes <- get_votes(paste0(i, ": "))
