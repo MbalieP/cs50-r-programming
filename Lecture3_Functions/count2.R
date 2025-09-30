696f2840680a8acef33026126f458f1d0d52906a
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-30 06:18:26 +0200

    Update count2.R

--- Lecture3_Functions/count2.R
+++ Lecture3_Functions/count2.R
@@ -1,12 +1,6 @@
 
 
 
- Ask for votes using the get_votes function and pass a custom prompt
-  votes <- get_votes(paste0(i, ": "))
-  
-  # Add the collected votes to the total
-    total <- total + votes
-}
 
 # Print out the total number of votes
 cat("Total votes:", total)
