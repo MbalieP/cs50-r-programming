dc53bc357a68e9862e09e5b4444979f52e236486
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-30 06:22:47 +0200

    Update duck.R

--- Lecture3_Functions/duck.R
+++ Lecture3_Functions/duck.R
@@ -1,4 +1,21 @@
 
+# ------------------------
+# Example 1: repeat loop (countdown version)
+# ------------------------
+
+i <- 3  # Initialize counter variable to 3
+
+repeat {
+  cat("quack! \n")  # Print "quack!" with a newline
+  i <- i - 1         # Decrease the value of i by 1
+  
+  # If i reaches 0, exit the loop
+  if (i == 0) {
+    break            # Exit the repeat loop
+  } else {
+    next             # Skip to the next iteration (not strictly needed here, as it goes to next anyway)
+  }
+}
 
 
 
