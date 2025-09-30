fead9d4f78b04269359dd570c99aa69315a28db2
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-30 06:15:57 +0200

    Update count2.R

--- Lecture3_Functions/count2.R
+++ Lecture3_Functions/count2.R
@@ -4,6 +4,12 @@ get_votes <- function(prompt = "Enter votes: ") {
   repeat {
     # Prompt the user and try to convert the input to an integer
     votes <- suppressWarnings(as.integer(readline(prompt)))
-  
+# If the conversion is successful (i.e., not NA), return the value
+    if (!is.na(votes)) {
+      return(votes)
+    }
+    # If the input is not a valid number, repeat the prompt
+  }
+}  
   
 
