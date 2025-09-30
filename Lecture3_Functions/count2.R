198ad7cf4c54e9935d2c7f7af6c3d20d66e9b5ec
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-09-30 06:10:23 +0200

    Update count2.R

--- Lecture3_Functions/count2.R
+++ Lecture3_Functions/count2.R
@@ -5,12 +5,10 @@ get_votes <- function(prompt = "Enter votes: ") {
       return(votes)
     }
   }
-  
 }
 
 for (i in c("Mario", "Peach","Bowser")){
   votes <-get_votes(paste0(i, ":"))
   total <- totat + votes
 }
-
 cat("Total votes:", total)
