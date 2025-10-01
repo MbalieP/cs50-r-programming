a0ac512eca7d21a16efa221ee6cfb2154f0161be
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-10-01 15:56:21 +0200

    Update tabulate.R

--- Lecture3_Functions/tabulate.R
+++ Lecture3_Functions/tabulate.R
@@ -1,25 +1,5 @@
 
-# Load the votes data from a CSV file into a dataframe
-votes <- read.csv("votes.csv")
-
-# Initialize an empty vector to store total votes for each candidate
-total_votes <- c()
-
-# Loop through each candidate (rows of the dataframe)
-for(candidate in rownames(votes)){
-  # For each candidate, calculate the sum of all their votes across methods (columns)
-  # Store this in the total_votes vector, with candidate name as the index
-  total_votes[candidate] <- sum(votes[candidate,])
-}
-
-# Add a new column 'total' to the votes dataframe containing each candidate's total votes
-votes$total <- total_votes
-
-
-# Reload the votes data
-votes <- read.csv("votes.csv")
-
-# Initialize an empty vector to store total votes for each method (column)
+#
 total_votes <- c()
 
 # Loop through each voting method (columns of the dataframe)
