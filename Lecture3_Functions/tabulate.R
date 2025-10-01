95665876bc17b2356e990fdf5aeffaf20b83116b
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-10-01 15:57:42 +0200

    Update tabulate.R

--- Lecture3_Functions/tabulate.R
+++ Lecture3_Functions/tabulate.R
@@ -1,20 +1,4 @@
 
 
-# Sort the total votes per method in ascending order
-sort(total_votes)
 
-# Sort the total votes per method in descending order
-sort(total_votes, decreasing = TRUE)
-
-# Work with rows (sum votes per candidate)
-votes <- read.csv("votes.csv")
-
-# IMPORTANT: This should be MARGIN = 1 for rows, not 2
-total_votes <- apply(votes, MARGIN = 1, FUN = sum)
-
-# Sort the total votes per candidate in ascending order
-sort(total_votes)
-
-# Sort the total votes per candidate in descending order
-sort(total_votes, decreasing = TRUE)
 
