04b755b9161aa74cac7c6003f670126b5d285bdc
Mbali Phulwane <mbphulwjhb024@student.wethinkcode.co.za>
2025-10-01 15:53:04 +0200

    Update tabulate.R

--- Lecture3_Functions/tabulate.R
+++ Lecture3_Functions/tabulate.R
@@ -1,9 +1,6 @@
 
 
-# functional programming
-#apply(votes,MARGIN =1, FUN =sum)  rows  ==  table names, rows/cols, function
-#apply(votes,MARGIN =2, FUN =sum)  columm
-
+#
 #COLUMN
 votes <- read.csv("cotes.csv")
 total_votes <- apply(votes, MARGIN = 2, FUN = sum)
